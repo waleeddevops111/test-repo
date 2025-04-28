425b7e3f731e98b7b0dd0d2f80b17197ac984f25
waleeddevops111 <devopswaleed@gmail.com>
2025-04-28 14:14:41 +0500

    script file

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run, the dragon is here" >> dragon.txt
 grep -i "dragon" dragon.txt
-echo "Running Same Workflow with a script"
+echo "Running Same Workflow with a script this time"
 ls -ltra
 cat dragon.txt
